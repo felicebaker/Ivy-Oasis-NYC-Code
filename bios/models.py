6a2b9b474caa923b411a23052955882187b748ee
Felice Baker <felicebaker2009@u.northwestern.edu>
2015-04-09 17:17:11 +0000

    Added Tiny MCE to bios admin using django-tinymce

--- bios/models.py
+++ bios/models.py
@@ -1,7 +1,7 @@
 # Create your models here.
 
 from django.db import models
-
+from tinymce import models as tinymce_models
 
 class Bios(models.Model):
 
@@ -9,7 +9,7 @@ class Bios(models.Model):
     bio_staff_position_short = models.CharField(max_length=100)
     bio_staff_position_long = models.CharField(max_length=150)
     bio_portrait = models.FileField(upload_to='/home/felicebaker/webapps/ivyoasisnyc/myproject/media/images/',max_length=255)
-    bio_education = models.TextField()
+    bio_education = tinymce_models.HTMLField()
     bio_company_name = models.CharField(max_length=150)
     bio_company_position = models.CharField(max_length=150)
     bio_website = models.CharField(max_length=150)
@@ -19,7 +19,7 @@ class Bios(models.Model):
     bio_phone_numbers = models.CharField(max_length=150)
     bio_email_address = models.CharField(max_length=150)
     bio_date_added = models.DateField()
-    bio_summary = models.TextField()
+    bio_summary = tinymce_models.HTMLField()
 
     class Meta:
             ordering = ['id']
